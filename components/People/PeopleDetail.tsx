d1285499d3d4586960d11b533a390e6cad147b51
Arian Derida <arianderida@gmail.com>
2021-04-08 13:06:41 +0700

    handle set data

--- components/People/PeopleDetail.tsx
+++ components/People/PeopleDetail.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import { Row, Col } from "antd";
 
 const PeopleDetail: React.FunctionComponent = ({ props }: any) => {
+  const { dataProfile } = props;
+  console.log(dataProfile);
   return (
     <div className="container">
       <div className="ppl-container">
@@ -10,21 +12,73 @@ const PeopleDetail: React.FunctionComponent = ({ props }: any) => {
             <div className="ppl-img-wrapper">
               <img src="/assets/avatar.jpg" alt="profile-photo" />
             </div>
-            <p className="ppl-info-r">Laki-laki</p>
-            <p className="ppl-info-r">28 September 1996</p>
+            <p className="ppl-info-r">
+              <b>
+                {dataProfile === "man"
+                  ? "Laki-laki"
+                  : dataProfile === "woman"
+                  ? "Perempuan"
+                  : "Lain-lain"}
+              </b>
+            </p>
+            <p className="ppl-info-r">
+              <b>{dataProfile?.date_of_birth ?? "Belum ada tanggal lahir"}</b>
+            </p>
           </Col>
           <Col span={16} className="ppl-info-wrapper">
-            <p>Nama: Admin Vascomm</p>
-            <p>Provinsi: Jawa Barat</p>
-            <p>Kota: Bogor</p>
-            <p>Kecamatan: Ciawi</p>
             <p>
-              Email: admin@vascomm.com{" "}
-              <b style={{ color: "red" }}>(unverified)</b>
+              Nama: <b>{dataProfile.firstname + " " + dataProfile.lastname}</b>
             </p>
-            <p>Telepon: 085725638278</p>
             <p>
-              Status: <b style={{ color: "green" }}>Aktif</b>
+              Provinsi:{" "}
+              <b>
+                {dataProfile.addr_province_code
+                  ? dataProfile.address_province.name
+                  : "Belum ada provinsi"}
+              </b>
+            </p>
+            <p>
+              Kota:{" "}
+              <b>
+                {dataProfile.addr_city_code
+                  ? dataProfile.address_city.name
+                  : "Belum ada kota"}
+              </b>
+            </p>
+            <p>
+              Kecamatan:{" "}
+              <b>
+                {dataProfile.addr_district_code
+                  ? dataProfile.address_district.name
+                  : "Belum ada kecamatan"}
+              </b>
+            </p>
+            <p>
+              Email: <b>{dataProfile?.email ?? "Belum ada email"}</b>{" "}
+              <b
+                style={
+                  !dataProfile.verified_at
+                    ? { color: "red" }
+                    : { color: "green" }
+                }
+              >
+                {!dataProfile.verified_at ? "(unverified)" : "(verified)"}
+              </b>
+            </p>
+            <p>
+              Telepon: <b>{dataProfile?.phone ?? "Belum ada telepon"}</b>
+            </p>
+            <p>
+              Status:{" "}
+              <b
+                style={
+                  dataProfile.status === 1
+                    ? { color: "green" }
+                    : { color: "red" }
+                }
+              >
+                {dataProfile.status === 1 ? "Aktif" : "Tidak Aktif"}
+              </b>
             </p>
           </Col>
         </Row>
--- pages/people/[id].tsx
+++ pages/people/[id].tsx
@@ -1,18 +1,75 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Layout from "../../components/Layout";
 import AuthLogout from "../../components/Auth/AuthLogout";
-
+import axios from "axios";
 import PeopleDetail from "../../components/People/PeopleDetail";
-const People: React.FunctionComponent = (props: any) => {
-  const { user } = props;
+import Router from "next/router";
+import { Spin } from "antd";
+
+const People = (props: any) => {
+  const { user, userid } = props;
+  const [dataProfile, setDataProfile] = useState<any>(null);
+
+  useEffect(() => {
+    if (userid) {
+      if (user.id === userid.id) {
+        Router.push({ pathname: `/people/${user.id}` });
+        setDataProfile(userid);
+      } else if (user.role === "admin") {
+        Router.push({ pathname: `/people/${userid.id}` });
+        setDataProfile(userid);
+      } else {
+        Router.push({ pathname: "/" });
+      }
+    } else {
+      Router.push({ pathname: "/" });
+    }
+  }, []);
+
+  console.log(dataProfile);
+
   return (
     <Layout title="People Detail | Test Vascomm">
       <div>
-        <PeopleDetail />
-        <AuthLogout props={{ user }} />
+        {!dataProfile ? (
+          <div
+            style={{ width: "100%", display: "flex", justifyContent: "center" }}
+          >
+            <Spin size="large" />
+          </div>
+        ) : (
+          <>
+            <PeopleDetail props={{ dataProfile }} />
+            <AuthLogout props={{ user }} />
+          </>
+        )}
       </div>
     </Layout>
   );
+};
+
+People.getInitialProps = async (ctx: any) => {
+  const { query } = ctx;
+  const { id } = query;
+  const userid: number = +id;
+
+  let user: any;
+
+  try {
+    user = await axios.get(`http://localhost:3000/api/people/${userid}`);
+
+    if (user.status === 200) {
+      delete user.data.data["password"];
+    } else {
+      user = null;
+    }
+  } catch (error) {
+    user = null;
+  }
+
+  console.log(user);
+
+  return { userid: user?.data?.data ?? null };
 };
 
 export default People;
